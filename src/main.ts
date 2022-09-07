a1b2c2d238331957048717a160af03159bd8208f
Ivan Herman <ivan@ivan-herman.net>
2022-09-07 16:39:32 +0200

    By default, the script picks up data online

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { Constants, TestSuite, TestDescription }          from './lib/common';
 import { convert_input, create_test_tables, modify_spec } from './lib/convert';
 import { JSDOM }                                          from "jsdom";
+import fetch                                              from "node-fetch";
+import { Command, OptionValues }                          from 'commander';
+
 
 import * as fs_old_school from "fs";
 const fs = fs_old_school.promises;
@@ -12,15 +15,24 @@ const fs = fs_old_school.promises;
  * 2. modify the specification tests by adding the test references following what respec requires
  */
 
-async function handleOneSpec(fname: string, data: TestSuite): Promise<void> {
+async function handleOneSpec(data: TestSuite, fname: string, local: boolean): Promise<void> {
     const fname_path: string[] = fname.split('/');
     if (fname_path.length != 0) {
         const base_name: string | undefined = fname_path.pop();
         const table_fragment_name: string = `${Constants.TABLE_FRAGMENTS}${base_name}`;
         const modified_fname =  base_name === 'index.html' ? `${Constants.SPEC_DIR}/index.html` :  `${Constants.VOCAB_DIR}/${base_name}`;
 
+        // Get the spec text
+        let src_text: string;
+
+        if (local) {
+            src_text = await fs.readFile(fname, 'utf-8');
+        } else {
+            const response = await fetch(fname);
+            src_text = await response.text(); 
+        }
+
         // Get the spec as a DOM
-        const src_text: string = await fs.readFile(fname, 'utf-8');
         const dom = new JSDOM(src_text);
         if (dom === null) {
             console.error(`${fname} could not be parsed`);
@@ -51,9 +63,24 @@ async function handleOneSpec(fname: string, data: TestSuite): Promise<void> {
  * 2. for each document to be modified and checked run `handleOneSpec` 
  */
 async function main(): Promise<void> {
+    const program = new Command();
+    program
+        .name('harvest epubcheck tests')
+        .description('Harvesting EPUB 3.3 specific test information to produce a test report for the EPUB 3.3 CR phase')
+        .usage('[options] [file]')
+        .option('-l, --local', 'pick up the specification files locally instead of fetching them from github')
+        .on('--help', () => {
+            console.log('  file:                      irc log file; if not present, retrieved from the W3C site');
+        })
+        .parse(process.argv);
+
+    const options: OptionValues = program.opts();
+    const local: boolean        = options.local ? true : false;
+    const sources: string[]     = local ? Constants.SPECS : Constants.SPECS_ONLINE ;
+
     const inp_data = await fs.readFile(Constants.TEST_DATA, 'utf-8');
     const data: TestSuite = JSON.parse(inp_data) as TestSuite;
-    await Promise.all(Constants.SPECS.map( (fname) => handleOneSpec(fname, data)));
+    await Promise.all(sources.map((fname) => handleOneSpec(data, fname, local)));
 }
 
 main();
